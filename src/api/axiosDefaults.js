84755efda7c4dce49abbdf94b3640053a78b9c9c
volausson <veronicagolausson@gmail.com>
2023-01-12 15:29:10 +0000

    rewrite code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,5 +4,3 @@ axios.defaults.baseURL = 'https://cic-api.herokuapp.com/';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
