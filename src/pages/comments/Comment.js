4e4d714c3628636fe6913605f6c6a9f58c7a5a3c
volausson <veronicagolausson@gmail.com>
2023-01-14 20:41:41 +0000

    add infinite scroll for comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,63 +1,80 @@
-import React from 'react'
-import { Media } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Avatar from '../../components/Avatar';
-import styles from '../../styles/Comment.module.css';
+import React, { useState } from "react";
+import { Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import CommentEditForm from "./CommentEditForm";
+
+import styles from "../../styles/Comment.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
 
 const Comment = (props) => {
-    const {
-        profile_id,
-        profile_image,
-        owner,
-        updated_at,
-        content,
-        id,
-        setPost,
-        setComments,
-    } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
 
-    const currentUser = useCurrentUser()
-    const is_owner = currentUser?.username === owner;
+  const [showEditForm, setShowEditForm] = useState(false);
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
-    const handleDelete = async () => {
-        try {
-          await axiosRes.delete(`/comments/${id}/`);
-          setPost((prevPost) => ({
-            results: [
-              {
-                ...prevPost.results[0],
-                comments_count: prevPost.results[0].comments_count - 1,
-              },
-            ],
-            }));
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
 
-            setComments((prevComments) => ({
-                ...prevComments,
-                results: prevComments.results.filter((comment) => comment.id !== id),
-              }));
-            } catch (err) {}
-          };
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
-    <div>
-        <hr />
-        <Media>
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image} />
-            </Link>
-            <Media.Body className='align-self-center ml-2'>
-                <span className={styles.Owner}>{owner}</span>
-                <span className={styles.Date}>{updated_at}</span>
-                <p>{content}</p>
-            </Media.Body>
-            {is_owner && (
-                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
-            )}
-        </Media>
-    </div>
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
+        </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
+    </>
   );
 };
 
