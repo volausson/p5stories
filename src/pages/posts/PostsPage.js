b4a92bd6f91d9d92338b696ef1a71f0cacfce3b4
volausson <veronicagolausson@gmail.com>
2023-01-15 11:14:02 +0000

    code check

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -18,34 +18,34 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 
-function PostsPage({message, filter = ' '}) {
-    const [posts, setPosts] = useState({ results: [] });
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const { pathname } = useLocation();
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
 
-    const [query, setQuery] = useState(false);
+  const [query, setQuery] = useState("");
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
-                setPosts(data);
-                setHasLoaded(true);
-            } catch (err) {
-                console.log(err);
-            }
-        };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        setHasLoaded(false);
-        const timer = setTimeout(() => {
-            fetchPosts();
-        }, 1000);
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [filter, query, pathname]);
-  
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
@@ -65,31 +65,31 @@ function PostsPage({message, filter = ' '}) {
         </Form>
 
         {hasLoaded ? (
-            <>
-                {posts.results.length ? (
-                    <InfiniteScroll
-                        children={posts.results.map((post) => (
-                            <Post key={post.id} {...post} setPosts={setPosts} />
-                        ))}
-                        dataLength={posts.results.length}
-                        loader={<Asset spinner />}
-                        hasMore={!!posts.next}
-                        next={() => fetchMoreData(posts, setPosts)}
-                    />
-                ) : (
-                    <Container className={appStyles.Content}>
-                        <Assets src={NoResults} message={message} />
-                    </Container>
-                )}
-            </>
+          <>
+            {posts.results.length ? (
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
         ) : (
-            <Container className={appStyles.Content}>
+          <Container className={appStyles.Content}>
             <Asset spinner />
-          </Container> 
+          </Container>
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
--- src/pages/posts/postPage.js
+++ src/pages/posts/postPage.js
@@ -1,51 +1,92 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
 import appStyles from "../../App.module.css";
-import Asset from "../../components/Asset";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+import Comment from "../comments/Comment";
+
+import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-const PopularProfiles = () => {
-  const [profileData, setProfileData] = useState({
-    // we will use the pageProfile later!
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const { popularProfiles } = profileData;
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
+
+function PostPage() {
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
   const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
     };
 
     handleMount();
-  }, [currentUser]);
+  }, [id]);
 
   return (
-    <Container className={appStyles.Content}>
-      {popularProfiles.results.length ? (
-        <>
-          <p>Most followed profiles.</p>
-          {popularProfiles.results.map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
-          ))}
-        </>
-      ) : (
-        <Asset spinner />
-      )}
-    </Container>
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
   );
-};
+}
 
-export default PopularProfiles;
+export default PostPage;
